e5554f38cd8affef105db28fe1070dac2c695d88
Álvaro Camacho <mauricio-xxi@hotmail.com>
2022-06-24 19:43:12 +0200

    refactor: move logic from enemy

--- enemy.go
+++ enemy.go
@@ -6,26 +6,15 @@ import (
 
 const basicEnemySize = 105
 
-type basicEnemy struct {
-	tex  *sdl.Texture
-	x, y float64
-}
+func newBasicEnemy(renderer *sdl.Renderer, position vector) *element {
+	basicEnemy := &element{}
+	basicEnemy.position = position
+	basicEnemy.rotation = 180
 
-func newBasicEnemy(renderer *sdl.Renderer, x, y float64) (be basicEnemy) {
-	be.tex = textureFromBMP(renderer, "sprites/basic_enemy.bmp")
-	be.x = x
-	be.y = y
-	return be
-}
+	sr := newSpriteRenderer(basicEnemy, renderer, "sprites/basic_enemy.bmp")
+	basicEnemy.addComponent(sr)
 
-func (be *basicEnemy) draw(renderer *sdl.Renderer) {
-	// transform the coordinates from top/left => center/center
-	x := be.x - basicEnemySize/2.0
-	y := be.y - basicEnemySize/2.0
-	renderer.CopyEx(be.tex,
-		&sdl.Rect{X: 0, Y: 0, W: basicEnemySize, H: basicEnemySize},
-		&sdl.Rect{X: int32(x), Y: int32(y), W: basicEnemySize, H: basicEnemySize},
-		180,
-		&sdl.Point{X: basicEnemySize / 2, Y: basicEnemySize / 2},
-		sdl.FLIP_NONE)
+	basicEnemy.active = true
+
+	return basicEnemy
 }
